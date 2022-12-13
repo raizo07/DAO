49be6d99a18ae4a77e3b751960ab4cda2487a541
raizo07 <agadaraizo@gmail.com>
2022-12-13 13:47:54 +0100

    compiled

--- hardhat-tutorial/contracts/FakeNFTMarketplace.sol
+++ hardhat-tutorial/contracts/FakeNFTMarketplace.sol
@@ -17,6 +17,13 @@ contract FakeNFTMarketplace {
         return nftPrice;
     }
 
-    
+    function available(uint256 _tokenId) external view returns (bool) {
+        // address(0) = 0x0000000000000000000000000000000000
+        // This is the default value for addresses in Solidity
+        if (tokens[_tokenId] == address(0)) {
+            return true;
+        }
+        return false;
+    }
 
 }
