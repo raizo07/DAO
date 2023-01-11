c4f9bb9fb375d6f25fa3fc4b9ffd54245d83cf3e
raizo07 <agadaraizo@gmail.com>
2023-01-11 05:24:21 +0100

    updates

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -11,4 +11,25 @@ import {
 } from "../constants";
 import styles from "../styles/Home.modules.css";
 
+export default function Home() {
+    const[treasuryBalance, setTreasurybalance] = useState("0");
+    const [numProposals, setNumberOfproposals] = useState("0");
+    const [proposals, setProposals] = useState([]);
+    const [nftBalance, setNftBalance] = useState("0");
+    const [fakeNftTokenId, setFakeNftTokenId] = useState("");
+    const [selectedTab, setSelectedTab] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [walletConnected, setWalletConnected] = useState(false);
+    const [isOwner, setIsOwner] = useState(false);
+    const web3Modal = useRef();
 
+    const connectWallet = async () => {
+        try {
+            await getProviderOrSigner();
+            setWalletConnected(true);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+}
